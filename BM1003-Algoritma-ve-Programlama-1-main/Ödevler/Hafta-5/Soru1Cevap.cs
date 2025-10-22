d7d60c9f234e44ef7c0ba0c77a242daaf86a61ea
yaylaci08008 <kaan.yaylaci6625@gmail.com>
2025-10-22 19:54:25 +0300

    Add files via upload

---
+++ BM1003-Algoritma-ve-Programlama-1-main/Ödevler/Hafta-5/Soru1Cevap.cs
@@ -0,0 +1,11 @@
+namespace Cevap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Çözümlerinizi buraya yazın.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
